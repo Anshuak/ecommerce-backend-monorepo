a1900487a44d6de7cbc284ba5567b2386a67d0de
Anshu Kailash <anshukailash.9924@gmail.com>
2025-12-27 09:12:09 +0530

    add category service interface

---
+++ product-service/src/main/java/com/shopsphere/product_service/service/CategoryService.java
@@ -0,0 +1,21 @@
+package com.shopsphere.product_service.service;
+
+import com.shopsphere.product_service.dto.CategoryResponseDto;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    // create a new category
+   public ResponseEntity<CategoryResponseDto> createCategory(CategoryResponseDto categoryResponseDto);
+
+   // get all categories
+    public ResponseEntity<List<CategoryResponseDto>> getAllCategories();
+
+    // get category by id
+    public ResponseEntity<CategoryResponseDto> getCategoryById(Long id);
+
+    // update category by id
+    public ResponseEntity<CategoryResponseDto> updateCategory(Long id, CategoryResponseDto categoryResponseDto);
+}
