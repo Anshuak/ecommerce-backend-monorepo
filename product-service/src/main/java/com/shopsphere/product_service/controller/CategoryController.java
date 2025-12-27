7f54f5026b9014f62296a74384a5c88865bb654f
Anshu Kailash <anshukailash.9924@gmail.com>
2025-12-27 09:06:15 +0530

    add model mapper

--- product-service/src/main/java/com/shopsphere/product_service/controller/CategoryController.java
+++ product-service/src/main/java/com/shopsphere/product_service/controller/CategoryController.java
@@ -1,5 +1,10 @@
 package com.shopsphere.product_service.controller;
 
+import com.shopsphere.product_service.dto.CategoryRequestDto;
+import com.shopsphere.product_service.dto.CategoryResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
 
     // post: create a new category (ADMIN only)
+    @PostMapping("/")
+    public ResponseEntity<CategoryResponseDto> createCategory(@RequestBody CategoryRequestDto categoryRequestDto) {
+        // Implementation goes here
+        return ResponseEntity.ok(new CategoryResponseDto());
+    }
 
     // get: retrieve all categories (public)
 
