1520973b06621f0a551bdd910468b593d9c6456c
Anshu Kailash <anshukailash.9924@gmail.com>
2025-12-27 08:41:22 +0530

    add category entity

---
+++ product-service/src/main/java/com/shopsphere/product_service/entity/Category.java
@@ -0,0 +1,42 @@
+package com.shopsphere.product_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "categories")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean active;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at", nullable = false)
+    private Date updatedAt;
+
+    @PrePersist
+    public void onCreate(){
+        Date now = new Date();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    public void onUpdate(){
+        this.updatedAt = new Date();
+    }
+
+}
